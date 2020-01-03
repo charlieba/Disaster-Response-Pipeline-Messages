f3b2d38ee0f546040e1bd1c3ff54e180f7a383bb
giovanib02 <giovanib07@gmail.com>
2020-01-03 19:03:56 +0000

    feat: adding training classifier flow

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,85 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+
+from sqlalchemy import create_engine
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+import pickle
 
 def load_data(database_filepath):
-    pass
-
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql("SELECT * FROM Messages_transformed", engine)
+    X = df.message.values
+    y = df.drop(columns=["id","message","original","genre","1"])
+    categories = y.columns.values 
+    return X, y, categories
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([('cvect', CountVectorizer(tokenizer = tokenize)),
+                     ('tfidf', TfidfTransformer()),
+                     ('clf', RandomForestClassifier())
+                     ])
+    parameters = {'clf__max_depth': [10, 20, None],
+              'clf__min_samples_leaf': [1, 2, 4],
+              'clf__min_samples_split': [2, 5, 10],
+              'clf__n_estimators': [10, 20, 40]}
+    cv = GridSearchCV(pipeline, param_grid=parameters, scoring='f1_micro', verbose=1, n_jobs=-1)
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    labels = np.unique(y_pred)
+    accuracy = (y_pred == Y_test).mean()
+
+    print("Labels:", labels)
+    print("Accuracy:", accuracy)
+
+    report = classification_report(y_test, y_pred, target_names = category_names)
+    print(report)
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as file:  
+        pickle.dump(model, file)
 
 
 def main():
